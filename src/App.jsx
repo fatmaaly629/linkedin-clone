6f3b0e098139f39bbafe9b5d2f268a611605fa8f
Fatma Aly <103731233+fatmaaly629@users.noreply.github.com>
2023-03-14 21:09:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { useEffect } from "react";
+import { getUserAuth } from "./redux/actions";
+import { connect } from "react-redux";
+import RequireAuth from "./components/RequireAuth";
+
+const App = (props) => {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <RequireAuth>
+                <Header />
+                <Home />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getUserAuth: () => dispatch(getUserAuth),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,45 @@
+// import { Children, useEffect } from "react";
+// import { connect } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+// const RequireAuth = ({ user, children }) => {
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     if (!user) {
+//       navigate("/", { replace: true });
+//       return;
+//     }
+//   }, [user]);
+
+//   return children;
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     user: state.userState.user,
+//   };
+// };
+
+// export default connect(mapStateToProps)(RequireAuth);
+
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const RequireAuth = ({ user, children }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/", { replace: true });
+      return;
+    }
+  }, [user]);
+  return children;
+};
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  };
+};
+
+export default connect(mapStateToProps)(RequireAuth);
